1653e3113e18ed467cacb2875a0f0c3a35ec36e6
Niloy-Sumon <niloyabidur@gmail.com>
2021-06-04 15:48:17 +0600

    integer learn with math

---
+++ conditional.js
@@ -0,0 +1,23 @@
+// var shareRate = 10;
+
+// if (shareRate === 10) {
+//     console.log("You can't win profit");   
+// }
+// else{
+//     console.log('You will go Home');
+// }
+// if (shareRate > 10) {
+//     console.log("You can't win profit");   
+// }
+// else{
+//     console.log('You will go Home');
+// }
+
+var job = true;
+var Merry = 300000;
+if (job === true && Merry > 200000) {
+    console.log("will you merry me");
+} else {
+    console.log("please don't come with me");
+}
+
