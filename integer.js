8318270277ecfb4069488896a6ef058ec90f50b4
Niloy-Sumon <niloyabidur@gmail.com>
2021-06-04 15:31:56 +0600

    integer learn with math

--- integer.js
+++ integer.js
@@ -1,4 +1,14 @@
-var num1 = 0.1;
-var num2 = 0.1;
-var total = num1 + num2
+// var num1 = 0.1; //Integer
+// var num2 = 0.2; //Float
+// var total = num1 + num2
+// var total = total.toFixed(2)
+// console.log(total);
+
+var price = 10;
+var price2 = 2;
+var total = price ** price2; //means 10*10
 console.log(total);
+
+
+{/* <	Less than */}
+// <=	Less than or equal
